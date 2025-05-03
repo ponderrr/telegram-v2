f558abc427e986b3f1d778a4e18672b1307d4165
VladF <vladimir.fiffiejr@proton.me>
2025-05-02 23:25:33 -0500

    add route handlers for authentication, messaging, and topics

---
+++ routes/authRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+constuserController = require("../controllers/userController");
+const auth = require("../middleware/auth");
+
+router.get("register", userController.getRegisterForm);
+router.post("/register", userController.register);
+router.get("/login", userController.getLoginForm);
+router.post("/login", userController.login);
+router.get("/logout", userController.logout);
+
+router.get("/dashboard", auth, userController.getDashboard);
+
+module.exports = router;
